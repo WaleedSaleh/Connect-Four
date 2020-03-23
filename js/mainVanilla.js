f583237c556c31957ef0e21ce9096e3a2e3682db
Waleed Saleh <saleh.waleed94@gmail.com>
2020-03-23 20:14:04 +0200

    Finished the logic for the row connection.

--- js/mainVanilla.js
+++ js/mainVanilla.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', onReady);
 //here we add to check the player who is playing
 var player = document.querySelector('#player');
 
+// initialize rows
+var rows = 6;
+var cols = 7;
+
 // Players flag
 var playerOneFlag = true;
 var playerTwoFlag = false;
@@ -12,6 +16,14 @@ var playerTwoFlag = false;
 // Store the result
 playerOneList = [];
 playerTwoList = [];
+
+
+// Creating two diminsional array/list
+for (var i = 0; i < rows; i++) {
+    playerOneList.push([]);
+    playerTwoList.push([]);
+}
+
 
 
 function checkNameAndColors(playerOne, playerOneColor, playeTwo, playerTwoColor) {
@@ -24,7 +36,6 @@ function checkNameAndColors(playerOne, playerOneColor, playeTwo, playerTwoColor)
     }
 
 }
-
 
 function onReady() {
     //Start by gettiing the users info
@@ -44,32 +55,81 @@ function onReady() {
     for (var i = 0; i < circles.length; i++) {
         circles[i].addEventListener('click', function() {
             if (playerOneFlag) {
-                playerOne();
-                playerTwoFlag = true;
-                playerOneFlag = false;
+                playerOne(this);
+                findWinner(playerOneList, playerTwoList, { name: "Player1" });
             } else if (playerTwoFlag) {
-                playerTwo();
-                playerOneFlag = true;
-                playerTwoFlag = false;
+                playerTwo(this);
+                findWinner(playerOneList, playerTwoList, { name: "Player2" });
             }
-
         })
     }
+
+    //findcircles(playerOneList, playerTwoList);
 }
 
-
-function playerOne() {
+function playerOne(circle) {
     player.textContent = "Waleed => Red";
-    this.setAttribute("fill", 'red');
-    var playerOneCircleId = this.getAttribute("id");
-    //console.log("Player 1: " + this.getAttribute("id"));
-    playerOneList.push(playerOneCircleId);
+    circle.setAttribute("fill", 'red');
+    row = circle.getAttribute("row");
+    col = circle.getAttribute("col");
+    playerOneList[row - 1].push(Number(col));
+    console.log("Added successfully to List 1");
+    playerTwoFlag = true;
+    playerOneFlag = false;
+
 }
 
 
-function playerTwo() {
+function playerTwo(circle) {
     player.textContent = "Salah => Green";
-    this.setAttribute('fill', 'green');
-    var playerTwoCircleId = this.getAttribute("id");
-    playerTwoList.push(playerTwoCircleId);
+    circle.setAttribute('fill', 'green');
+    row = circle.getAttribute("row");
+    col = circle.getAttribute("col");
+    console.log("Row: " + row + " Col: " + col);
+    playerTwoList[row - 1].push(Number(col));
+    console.log("Added successfully to List 2");
+    playerTwoFlag = false;
+    playerOneFlag = true;
+}
+
+var count1 = 0;
+var count2 = 0;
+
+function findWinner(list1, list2, opt) {
+    if (opt['name'] === "Player1") {
+        console.log("Player 1");
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < list1[i].length; j++) {
+                if (list1[i].length > 1) {
+                    if (list1[i][j] === list1[i][j + 1] - 1) {
+                        console.log("entered Rows List 1");
+                        count1++;
+                    } else if (list1[i][j] === list1[i][j]) {
+                        console.log("entered Cols List 1");
+                    }
+                }
+            }
+        }
+    } else {
+        console.log("Player 2");
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < list1[i].length; j++) {
+                if (list2[i].length > 1) {
+                    if (list2[i][j] === list2[i][j + 1] - 1) {
+                        console.log("entered List 1");
+                        count2++;
+                    }
+                }
+            }
+        }
+    }
+    if (count1 >= 4) {
+        alert("Player 1 Wins");
+        player.textContent = "Player 1 wins, to start again please refresh the page :)"
+    } else if (count2 >= 4) {
+        alert("Player 2 wins!");
+        player.textContent = "Player 2 wins, to start again please refresh the page :)"
+    }
+    console.log("Count 1: " + count1);
+    console.log("Count 2: " + count2);
 }
